1dec694b5fdc739dd76638be5686971d3bea1b3e
Jonathan Benson <jbensonamdg@gmail.com>
2022-04-26 17:40:46 -0500

    Fixed bug in JOIN_TABLE procedure

--- procedures.sql
+++ procedures.sql
@@ -7,9 +7,11 @@ DELIMITER //
 CREATE PROCEDURE CLEAN ()
 BEGIN
 
+    DELETE FROM SEAT;
     DELETE FROM ROUND;
     DELETE FROM _MATCH;
     DELETE FROM _TABLE;
+    DELETE FROM _USER;
 
     UPDATE SEAT
     SET SitterUsername=NULL;
@@ -38,27 +40,27 @@ CREATE PROCEDURE JOIN_TABLE (In playerName varchar(255), In pokerTable INT, OUT 
 BEGIN
 
     SET @playerId = NULL;
-    SET @tableId = NULL;
-    
+    SET @seatId = NULL;
+
     #fetch playerId of that player from player table using player name or 
     #else store player name directly into the pokerTable so direct check that player present in that table or not
-    SELECT Username into @playerId FROM _USER WHERE Username=playerName;
+    SELECT Username INTO @playerId FROM _USER WHERE Username=playerName;
     
     #check that player present in that table or not
     SELECT _Index INTO @seatId
     FROM SEAT
     INNER JOIN _TABLE ON _TABLE.TableId=pokerTable
-    WHERE player_id=@playerId
+    WHERE SEAT.SitterUsername=@playerId
     LIMIT 1;
     
     #if seat id is null it means that player have no seat in that table, then check is there any seats empty
     if(@seatId IS NULL)THEN
     BEGIN
 
-        SELECT _Index into @seatId
+        SELECT _Index INTO @seatId
         FROM SEAT
         INNER JOIN _TABLE ON _TABLE.TableId=pokerTable
-        WHERE SEAT.SitterUsername = NULL
+        WHERE SEAT.SitterUsername IS NULL
         LIMIT 1;
 
         #if seat id is not null it means empty seat is present then give that seat to that player
@@ -66,7 +68,7 @@ BEGIN
         BEGIN
             UPDATE SEAT
             SET SitterUsername=@playerId
-            WHERE _Index=@seatId;
+            WHERE _Index=@seatId AND TableId=pokerTable;
 
             SET msg='SUCCESS';
         END;
